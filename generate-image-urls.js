d96d395b0bfcf7a51cd0b45505255e7515bafd31
Chino <69903799+kafuneri@users.noreply.github.com>
2025-04-18 10:14:04 +0800

    Update generate-image-urls.js

--- generate-image-urls.js
+++ generate-image-urls.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const imageBaseUrl = "https://random-picture.kafuchino.top/images";
+const imageBaseUrl = "https://image.api.kafuchino.top/images";
 const apiFilePath = path.join("functions", "api.js");
 const indexHtmlPath = path.join("images", "index.html");
 const rootDir = path.join(process.cwd(), "images");
